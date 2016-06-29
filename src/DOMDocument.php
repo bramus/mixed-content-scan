34567ffdad0cdd91775a41dad70be41486458f71
Jamie Burchell <jamieburchell@yahoo.co.uk>
2016-06-29 17:35:02 +0100

    Array of tags and attributes to keep things DRY

--- src/DOMDocument.php
+++ src/DOMDocument.php
@@ -8,6 +8,20 @@ namespace Bramus\MCS;
  */
 class DOMDocument extends \DOMDocument
 {
+    public $tags = [
+        'audio'  => ['src'],
+        'embed'  => ['src'],
+        'form'   => ['action'],
+        'iframe' => ['src'],
+        'img'    => ['src', 'srcset'],
+        'link'   => ['href'],
+        'object' => ['data'],
+        'param'  => ['value'],
+        'script' => ['src'],
+        'source' => ['src', 'srcset'],
+        'video'  => ['src']
+    ];
+
     public function extractLinks()
     {
         $links = [];
@@ -28,124 +42,17 @@ class DOMDocument extends \DOMDocument
         // We'll return this one at the very end
         $mixedContentUrls = [];
 
-        // Check all iframes contained in the HTML
-        foreach ($this->getElementsByTagName('iframe') as $el) {
-            if ($el->hasAttribute('src')) {
-                $url = $el->getAttribute('src');
-                if (substr($url, 0, 7) == "http://") {
-                    $mixedContentUrls[] = $url;
-                }
-            }
-        }
-
-        // Check all images contained in the HTML
-        foreach ($this->getElementsByTagName('img') as $el) {
-            if ($el->hasAttribute('src')) {
-                $url = $el->getAttribute('src');
-                if (substr($url, 0, 7) == "http://") {
-                    $mixedContentUrls[] = $url;
-                }
-            }
-            if ($el->hasAttribute('srcset')) {
-                $url = $el->getAttribute('srcset');
-                if (stripos($url, "http://") !== false) {
-                    $mixedContentUrls[] = $url;
-                }
-            }
-        }
-
-        // Check all script elements contained in the HTML
-        foreach ($this->getElementsByTagName('script') as $el) {
-            if ($el->hasAttribute('src')) {
-                $url = $el->getAttribute('src');
-                if (substr($url, 0, 7) == "http://") {
-                    $mixedContentUrls[] = $url;
-                }
-            }
-        }
-
-        // Check all stylesheet links contained in the HTML
-        foreach ($this->getElementsByTagName('link') as $el) {
-            if ($el->hasAttribute('href') && $el->hasAttribute('rel') && ($el->getAttribute('rel') == 'stylesheet')) {
-                $url = $el->getAttribute('href');
-                if (substr($url, 0, 7) == "http://") {
-                    $mixedContentUrls[] = $url;
-                }
-            }
-        }
-
-        // Check all `object` elements contained in the HTML
-        foreach ($this->getElementsByTagName('object') as $el) {
-            if ($el->hasAttribute('data')) {
-                $url = $el->getAttribute('data');
-                if (substr($url, 0, 7) == "http://") {
-                    $mixedContentUrls[] = $url;
-                }
-            }
-        }
-
-        // Check all `embed` elements contained in the HTML
-        foreach ($this->getElementsByTagName('embed') as $el) {
-            if ($el->hasAttribute('src')) {
-                $url = $el->getAttribute('src');
-                if (substr($url, 0, 7) == "http://") {
-                    $mixedContentUrls[] = $url;
-                }
-            }
-        }
-
-        // Check all `video` elements contained in the HTML
-        foreach ($this->getElementsByTagName('video') as $el) {
-            if ($el->hasAttribute('src')) {
-                $url = $el->getAttribute('src');
-                if (substr($url, 0, 7) == "http://") {
-                    $mixedContentUrls[] = $url;
-                }
-            }
-        }
-
-        // Check all `audio` elements contained in the HTML
-        foreach ($this->getElementsByTagName('audio') as $el) {
-            if ($el->hasAttribute('src')) {
-                $url = $el->getAttribute('src');
-                if (substr($url, 0, 7) == "http://") {
-                    $mixedContentUrls[] = $url;
-                }
-            }
-        }
-
-        // Check all `source` elements contained in the HTML
-        foreach ($this->getElementsByTagName('source') as $el) {
-            if ($el->hasAttribute('src')) {
-                $url = $el->getAttribute('src');
-                if (substr($url, 0, 7) == "http://") {
-                    $mixedContentUrls[] = $url;
-                }
-            }
-            if ($el->hasAttribute('srcset')) {
-                $url = $el->getAttribute('srcset');
-                if (stripos($url, "http://") !== false) {
-                    $mixedContentUrls[] = $url;
-                }
-            }
-        }
-
-        // Check all `param` elements contained in the HTML
-        foreach ($this->getElementsByTagName('param') as $el) {
-            if ($el->hasAttribute('value') && $el->hasAttribute('name') && ($el->getAttribute('name') == 'movie')) {
-                $url = $el->getAttribute('value');
-                if (substr($url, 0, 7) == "http://") {
-                    $mixedContentUrls[] = $url;
-                }
-            }
-        }
-
-        // Check all `form` elements contained in the HTML
-        foreach ($this->getElementsByTagName('form') as $el) {
-            if ($el->hasAttribute('action')) {
-                $url = $el->getAttribute('action');
-                if (substr($url, 0, 7) == "http://") {
-                    $mixedContentUrls[] = $url;
+        // Loop through all the tags and attributes we want to find
+        // references to mixed content
+        foreach ($this->tags as $tag => $attributes) {
+            foreach ($this->getElementsByTagName($tag) as $el) {
+                foreach ($attributes as $attribute) {
+                    if ($el->hasAttribute($attribute)) {
+                        $url = $el->getAttribute($attribute);
+                        if (stripos($url, 'http://') !== false) {
+                            $mixedContentUrls[] = $url;
+                        }
+                    }
                 }
             }
         }
